2b0de02c338533fc21b336d1885291d05e8ecd47
John <jldjld503@gmail.com>
2016-05-31 12:03:18 -0700

    added option for adding a new task

--- app/edit-task-details.component.ts
+++ app/edit-task-details.component.ts
@@ -5,7 +5,10 @@ import {Task} from './task.model';
   selector: 'edit-task-details',
   inputs: ['task'],
   template: `
-    <input [(ngModel)]="task.description"/>
+    <div class="task-form">
+      <h3>Edit Description: </h3>
+      <input [(ngModel)]="task.description" class="col-sm-8 input-lg task-form"/>
+    </div>
   `
 })
 export class EditTaskDetailsComponent {
